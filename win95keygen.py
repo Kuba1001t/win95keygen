8bc6e82072cb749df944e450b838ea2151d5109d
intru <49855794+intruzek3@users.noreply.github.com>
2021-02-10 18:22:30 +0100

    code optimization
    Version 1.1

--- win95keygen.py
+++ win95keygen.py
@@ -1,47 +1,30 @@
+#version 1.1
 import random
 import sys
+#10-digits CD Key
 def a10d():
 	p1=random.randint(0,999)
 	while p1==333 or p1==444 or p1==555 or p1==666 or p1==777 or p1==888 or p1==999:
 		p1=random.randint(0,999)
-	code1=str(p1)
-	if p1<10:
-		code1="0"+"0"+code1
-	elif p1<100:
-		code1="0"+code1	
+	code1=str(p1).zfill(3)
 	def rng():
 		global p2
 		global code2
 		global p2sum
 		p2=random.randint(1,9999999)
-		code2=str(p2)
+		code2=str(p2).zfill(7)
 		p2sum=0
 		for i in code2:
 			cyfra1=int(i)
 			p2sum+=cyfra1
-		if p2<10:
-			code2="0"+"0"+"0"+"0"+"0"+"0"+code2
-		elif p2<100:
-			code2="0"+"0"+"0"+"0"+"0"+code2
-		elif p2<1000:
-			code2="0"+"0"+"0"+"0"+code2
-		elif p2<10000:
-			code2="0"+"0"+"0"+code2
-		elif p2<100000:
-			code2="0"+"0"+code2
-		elif p2<1000000:
-			code2="0"+code2
 	rng()
 	while p2sum%7!=0 or int(code2[6])==0 or int(code2[6])>=8:
 		rng()
 	print(code1+"-"+code2)
+#11-Digits CD Key
 def a11d():
 	p1=random.randint(1,999)
-	code1=str(p1)
-	if p1<10:
-		code1="0"+"0"+code1
-	elif p1<100:
-		code1="0"+code1
+	code1=str(p1).zfill(3)
 	rand=random.randint(1,2)
 	p1ost=int(code1[2])+rand
 	if p1ost==11:
@@ -54,70 +37,37 @@ def a11d():
 		global code2
 		global p2sum
 		p2=random.randint(1,9999999)
-		code2=str(p2)
+		code2=str(p2).zfill(7)
 		p2sum=0
 		for i in code2:
 			cyfra1=int(i)
 			p2sum+=cyfra1
-		if p2<10:
-			code2="0"+"0"+"0"+"0"+"0"+"0"+code2
-		elif p2<100:
-			code2="0"+"0"+"0"+"0"+"0"+code2
-		elif p2<1000:
-			code2="0"+"0"+"0"+"0"+code2
-		elif p2<10000:
-			code2="0"+"0"+"0"+code2
-		elif p2<100000:
-			code2="0"+"0"+code2
-		elif p2<1000000:
-			code2="0"+code2
 	rng()
 	while p2sum%7!=0 or int(code2[6])==0 or int(code2[6])>=8:
 		rng()
 	print(code1+"-"+code2)
+#OEM Key
 def oem():
 	p1p1=random.randint(1,366)
 	p1p2=random.choice(['95','96','97','98','99','00','01','02','03'])
-	code1=str(p1p1)+p1p2
-	if p1p1<10:
-		code1="0"+"0"+code1
-	elif p1p1<100:
-		code1="0"+code1
+	code1=(str(p1p1)+p1p2).zfill(5)
 	def rng():
 		global p2
 		global code2
 		global p2sum
 		p2=random.randint(1,999999)
-		code2=str(p2)
+		code2=str(p2).zfill(7)
 		p2sum=0
 		for i in code2:
 			cyfra1=int(i)
 			p2sum+=cyfra1
-		if p2<10:
-			code2="0"+"0"+"0"+"0"+"0"+code2
-		elif p2<100:
-			code2="0"+"0"+"0"+"0"+code2
-		elif p2<1000:
-			code2="0"+"0"+"0"+code2
-		elif p2<10000:
-			code2="0"+"0"+code2
-		elif p2<100000:
-			code2="0"+code2
-		code2="0"+code2
 	rng()
 	while p2sum%7!=0 or int(code2[5])==0 or int(code2[5])>=8:
 		rng()
 	p3=random.randint(1,99999)
-	code3=str(p3)
-	if p3<10:
-		code3="0"+"0"+"0"+"0"+code3
-	elif p3<100:
-		code3="0"+"0"+"0"+code3
-	elif p3<1000:
-		code3="0"+"0"+code3
-	elif p3<10000:
-		code3="0"+code3
+	code3=str(p3).zfill(5)
 	print(code1+"-"+"OEM"+"-"+code2+"-"+code3)
+
 if len(sys.argv)>1:	
 	if sys.argv[1]=="-10d":
 		if len(sys.argv)>2:
