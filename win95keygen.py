39cdaefb999195d671823078e18f46863d765d97
Kuba Turzański <kuba1001@protonmail.com>
2022-06-12 22:24:11 +0200

    Move rng function out of other functions

--- win95keygen.py
+++ win95keygen.py
@@ -1,13 +1,9 @@
 #version 1.1
 import random
 import sys
-#10-digits CD Key
-def a10d():
-	p1=random.randint(0,999)
-	while p1==333 or p1==444 or p1==555 or p1==666 or p1==777 or p1==888 or p1==999:
-		p1=random.randint(0,999)
-	code1=str(p1).zfill(3)
-	def rng():
+
+#random 7 digits
+def rng():
 		global p2
 		global code2
 		global p2sum
@@ -17,10 +13,18 @@ def a10d():
 		for i in code2:
 			cyfra1=int(i)
 			p2sum+=cyfra1
+
+#10-digits CD Key
+def a10d():
+	p1=random.randint(0,999)
+	while p1==333 or p1==444 or p1==555 or p1==666 or p1==777 or p1==888 or p1==999:
+		p1=random.randint(0,999)
+	code1=str(p1).zfill(3)
 	rng()
 	while p2sum%7!=0 or int(code2[6])==0 or int(code2[6])>=8:
 		rng()
 	print(code1+"-"+code2)
+
 #11-Digits CD Key
 def a11d():
 	p1=random.randint(1,999)
@@ -32,35 +36,16 @@ def a11d():
 	elif p1ost==10:
 		p1ost=0
 	code1+=str(p1ost)
-	def rng():
-		global p2
-		global code2
-		global p2sum
-		p2=random.randint(1,9999999)
-		code2=str(p2).zfill(7)
-		p2sum=0
-		for i in code2:
-			cyfra1=int(i)
-			p2sum+=cyfra1
 	rng()
 	while p2sum%7!=0 or int(code2[6])==0 or int(code2[6])>=8:
 		rng()
 	print(code1+"-"+code2)
+
 #OEM Key
 def oem():
 	p1p1=random.randint(1,366)
 	p1p2=random.choice(['95','96','97','98','99','00','01','02','03'])
 	code1=(str(p1p1)+p1p2).zfill(5)
-	def rng():
-		global p2
-		global code2
-		global p2sum
-		p2=random.randint(1,999999)
-		code2=str(p2).zfill(7)
-		p2sum=0
-		for i in code2:
-			cyfra1=int(i)
-			p2sum+=cyfra1
 	rng()
 	while p2sum%7!=0 or int(code2[5])==0 or int(code2[5])>=8:
 		rng()
