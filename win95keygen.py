4ff1f6fa061e6e30d22ea34d3b17cb269ff4f630
Kuba Turzański <kuba1001@protonmail.com>
2022-06-13 19:11:56 +0200

    Fix code and move checks of rng fn to itself

--- win95keygen.py
+++ win95keygen.py
@@ -4,12 +4,16 @@ import sys
 
 #random 7 digits
 def rng():
-	p2=random.randint(1,9999999)
-	code2=str(p2).zfill(7)
-	p2sum=0
-	for i in code2:
-		cyfra1=int(i)
-		p2sum+=cyfra1
+	while True:
+		p2=random.randint(1,9999999)
+		code2=str(p2).zfill(7)
+		p2sum=0
+		for i in code2:
+			cyfra1=int(i)
+			p2sum+=cyfra1
+		if !(p2sum%7!=0 or int(code2[6])==0 or int(code2[6])>=8:) {
+			return code2
+		}
 
 #10-digits CD Key
 def a10d():
@@ -17,9 +21,7 @@ def a10d():
 	while p1==333 or p1==444 or p1==555 or p1==666 or p1==777 or p1==888 or p1==999:
 		p1=random.randint(0,999)
 	code1=str(p1).zfill(3)
-	rng()
-	while p2sum%7!=0 or int(code2[6])==0 or int(code2[6])>=8:
-		rng()
+	code2 = rng()
 	print(code1+"-"+code2)
 
 #11-Digits CD Key
@@ -33,9 +35,7 @@ def a11d():
 	elif p1ost==10:
 		p1ost=0
 	code1+=str(p1ost)
-	rng()
-	while p2sum%7!=0 or int(code2[6])==0 or int(code2[6])>=8:
-		rng()
+	code2 = rng()
 	print(code1+"-"+code2)
 
 #OEM Key
@@ -43,9 +43,7 @@ def oem():
 	p1p1=random.randint(1,366)
 	p1p2=random.choice(['95','96','97','98','99','00','01','02','03'])
 	code1=(str(p1p1)+p1p2).zfill(5)
-	rng()
-	while p2sum%7!=0 or int(code2[5])==0 or int(code2[5])>=8:
-		rng()
+	code2 = rng()
 	p3=random.randint(1,99999)
 	code3=str(p3).zfill(5)
 	print(code1+"-"+"OEM"+"-"+code2+"-"+code3)
